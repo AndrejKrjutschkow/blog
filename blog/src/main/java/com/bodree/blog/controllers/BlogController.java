aaf2542b87966ab4ea58ee5c4898ef487b7c534a
Andrej <andrejkrjutchkow@gmail.com>
2021-11-21 01:39:45 +0300

    add page blog-details.html

--- blog/src/main/java/com/bodree/blog/controllers/BlogController.java
+++ blog/src/main/java/com/bodree/blog/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -35,5 +39,16 @@ public class BlogController {
         Post post = new Post(title, anounce, fullText);
         postRepository.save(post);//отправка на сервер
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")//динамическое получение номера страницы
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if (!postRepository.existsById(id)) return "redirect:/blog";
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
     }
 }
