e8d6fa9e9cb675e2b2c177e8e69d1e10b5edfb23
Andrej <andrejkrjutchkow@gmail.com>
2021-11-20 22:20:08 +0300

    Initial commit

---
+++ blog/src/test/java/com/bodree/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodree.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
